285e2a73876850fe1395349e259c6038172027a6
laxmichaithanya <laxmichaithanya2306@gmail.com>
2026-07-10 14:34:41 +0530

    Point frontend to live backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://wanderstay-backend-it7d.onrender.com/api",
+
 });
 
 api.interceptors.request.use((config) => {
